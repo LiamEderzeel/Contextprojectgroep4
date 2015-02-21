dec09c7ef321887e0460b4f506ffb21bbf90ab8d
Liam Ederzeel <liamederzeel@gmail.com>
2015-02-21 18:26:37 +0100

    adding reources bij mouse click

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -14,8 +14,27 @@ public class Player : MonoBehaviour {
 	}	
 	// Update is called once per frame
 	void Update () {
-
+		if (Input.GetMouseButtonDown(0)) {
+			Debug.Log("Pressed left click, casting ray.");
+			CastRay();
+		}
 	}
+	void CastRay() {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, 100)) {
+			Debug.DrawLine(ray.origin, hit.point);
+			Debug.Log("Hit object: " + hit.collider.name);
+			if(hit.collider.name == "Resource_1") {
+				resource_1 ++;
+			} else if (hit.collider.name == "Resource_2"){
+				resource_2 ++;
+			} else if (hit.collider.name == "Resource_3"){
+				resource_3 ++;
+			}
+		}
+	}
+
 
 	void OnGUI () {
 		GUI.Label (new Rect (0,0,100,50), "Graan " + resource_1);
