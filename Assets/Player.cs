a748cad932f4ce1e9f7c5a290eae6fc00fd48525
Liam Ederzeel <liamederzeel@gmail.com>
2015-02-25 09:02:43 +0100

    Removed old code

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -35,65 +35,6 @@ public class Player : MonoBehaviour
 				resource_2 ++;
 			else if (HitGrondstof.ResourceType == Resource.Grondstof.Water)
 				resource_3 ++;
-/*
-			if(hit.collider.name == "City_1")
-			{
-				if(resource_1 > 0 && City.City1RequestResource_1 > 0)
-				{
-					resource_1 = resource_1 - City.City1RequestResource_1;
-				}
-				if(resource_2 > 0 && City.City1RequestResource_2 > 0)
-				{
-					resource_2 = resource_2 - City.City1RequestResource_2;
-				}
-				if(resource_3 > 0 && City.City1RequestResource_3 > 0)
-				{
-					resource_3 = resource_3 - City.City1RequestResource_3;
-				}
-				//City.City1RequestResource_1 = 0;
-				//City.City1RequestResource_2 = 0;
-				//City.City1RequestResource_3 = 0;
-				Debug.Log("RequestResource_1" + City.City1RequestResource_1);
-				Debug.Log("RequestResource_2" + City.City1RequestResource_2);
-				Debug.Log("RequestResource_3" + City.City1RequestResource_3);
-			}
-			else if (hit.collider.name == "City_2")
-			{
-				if(resource_1 > 0 && City.City2RequestResource_1 > 0)
-				{
-					resource_1 = resource_1 - City.City2RequestResource_1;
-				}
-				else if(resource_2 > 0 && City.City2RequestResource_2 > 0)
-				{
-					resource_2 = resource_2 - City.City2RequestResource_2;
-				}
-				else if(resource_3 > 0 && City.City2RequestResource_3 > 0)
-				{
-					resource_3 = resource_3 - City.City2RequestResource_3;
-				}
-				//City.City2RequestResource_1 = 0;
-				//City.City2RequestResource_2 = 0;
-				//City.City2RequestResource_3 = 0;
-			}
-			else if (hit.collider.name == "City_3")
-			{
-				if(resource_1 > 0 && City.City3RequestResource_1 > 0)
-				{
-					resource_1 = resource_1 - City.City3RequestResource_1;
-				}
-				else if(resource_2 > 0 && City.City3RequestResource_2 > 0)
-				{
-					resource_2 = resource_2 - City.City3RequestResource_2;
-				}
-				else if(resource_3 > 0 && City.City3RequestResource_3 > 0)
-				{
-					resource_3 = resource_3 - City.City3RequestResource_3;
-				}
-				//City.City3RequestResource_1 = 0;
-				//City.City3RequestResource_2 = 0;
-				//City.City3RequestResource_3 = 0;
-			}
-			*/
 		}
 	}
 	void Update ()
