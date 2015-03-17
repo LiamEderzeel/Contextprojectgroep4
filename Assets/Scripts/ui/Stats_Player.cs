d3761952ae73e015b66cc8d47fd598ac0e119c70
ignazkevenaar <ignazkevenaar@gmail.com>
2015-03-17 01:55:43 +0100

    inventory waarden gelinkt aan schild

---
+++ Assets/Scripts/ui/Stats_Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stats_Player : MonoBehaviour {
+
+	public GameObject CounterVoedsel;
+	public GameObject CounterTextiel;
+	public GameObject CounterSteenkool;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CounterVoedsel.GetComponent<doubleDigitCounter> ().Value = Player.resource_1;
+		CounterTextiel.GetComponent<doubleDigitCounter> ().Value = Player.resource_2;
+		CounterSteenkool.GetComponent<doubleDigitCounter> ().Value = Player.resource_3;
+	}
+}
