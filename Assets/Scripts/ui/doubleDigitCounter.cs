0200bf367191a3de422c9cc7ec5795623f7ad1e6
ignazkevenaar <ignazkevenaar@gmail.com>
2015-03-17 01:44:50 +0100

    werkende 2 digit teller

---
+++ Assets/Scripts/ui/doubleDigitCounter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doubleDigitCounter : MonoBehaviour {
+
+	public int Value;
+	public string DigitFolder;
+
+	//private
+
+	Sprite[] imageCollection = new Sprite[10];
+
+	GameObject numLow;
+	GameObject numHigh;
+
+
+	// Use this for initialization
+	void Start () {
+		//gameobjects van de nummers zoeken zodat we de sprites kunnen veranderen
+		numLow = this.gameObject.transform.FindChild("numLow").gameObject;
+		numHigh = this.gameObject.transform.FindChild("numHigh").gameObject;
+
+		//plaatjes inladen voor het font
+		for (int i=0; i < 10; i++)
+			imageCollection [i] = Resources.Load<Sprite> ("Sprites/ui/font_hud/" + i);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//waarde van 2 digits kan max 99 zijn
+		Mathf.Clamp (Value, 0, 99);
+		//forex 25
+		int intLow  = Value % 10; //5
+		int intHigh = Value / 10; //2
+		numLow.GetComponent<SpriteRenderer> ().sprite = imageCollection [intLow];
+		numHigh.GetComponent<SpriteRenderer> ().sprite = imageCollection [intHigh];
+	}
+}
