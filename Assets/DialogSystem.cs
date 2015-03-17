c7a74f56a940130011d7b0b2e858aa6b85a899f8
ignazkevenaar <ignazkevenaar@gmail.com>
2015-03-17 15:01:28 +0100

    dialog collection

---
+++ Assets/DialogSystem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogSystem : MonoBehaviour {
+
+
+	ArrayList Dialogs = new ArrayList();
+
+	// Use this for initialization
+	void Start () {
+		//inladen van elke 
+		foreach (Transform child in this.gameObject.transform) {
+			Dialogs.Add (child.gameObject);
+			child.gameObject.SetActive(false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
