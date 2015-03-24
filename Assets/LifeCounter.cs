ad977a8d2d2854ce8575bf86d5c185d01d089722
ignazkevenaar <ignazkevenaar@gmail.com>
2015-03-24 11:03:19 +0100

    steden hp counter

---
+++ Assets/LifeCounter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeCounter : MonoBehaviour {
+
+	public City AttachedCity;
+
+	GameObject bar;
+
+	// Use this for initialization
+	void Start () {
+		bar = transform.Find ("life_high").gameObject;
+		//if (!AttachedCity)
+		AttachedCity = this.gameObject.transform.parent.gameObject.GetComponent<City> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float percentage = (float)AttachedCity.CityHP / 100f;
+		bar.transform.localScale = new Vector3(percentage, 1, 1);
+		Debug.Log (percentage);
+	}
+}
