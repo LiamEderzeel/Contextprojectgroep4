50f074c58861e311451195172d93a54a61ad2764
ignazkevenaar <ignazkevenaar@gmail.com>
2015-03-24 11:10:31 +0100

    city stats zijn nu children van cities

--- Assets/Scripts/Stats_City.cs
+++ Assets/Scripts/Stats_City.cs
@@ -3,8 +3,7 @@ using System.Collections;
 
 public class Stats_City : MonoBehaviour {
 
-	public GameObject watchedCity;
-	private City c;
+	public City AttachedCity;
 
 	//badges
 	private SpriteRenderer bT;
@@ -15,28 +14,27 @@ public class Stats_City : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		c = watchedCity.GetComponent<City> ();
-
 		bT = this.gameObject.transform.FindChild ("badgeT").gameObject.GetComponent<SpriteRenderer> ();
 		bO = this.gameObject.transform.FindChild ("badgeO").gameObject.GetComponent<SpriteRenderer> ();
 		cT = this.gameObject.transform.FindChild ("counterT").gameObject.GetComponent<doubleDigitCounter> ();
 		cO = this.gameObject.transform.FindChild ("counterO").gameObject.GetComponent<doubleDigitCounter> ();
-
+		if (!AttachedCity)
+			AttachedCity = this.gameObject.transform.parent.gameObject.GetComponent<City> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	 	cT.Value = c.rc.Tekort;
-	 	cO.Value = c.rc.Overschot;
+		cT.Value = AttachedCity.rc.Tekort;
+		cO.Value = AttachedCity.rc.Overschot;
 		Sprite sT = Resources.Load<Sprite> ("Sprites/voedsel");
 		Sprite sO = Resources.Load<Sprite> ("Sprites/voedsel");
 
-		if (c.rc.Tekort > 0) {
-			if (c.rc.TekortType == Player.Grondstof.Voedsel)
+		if (AttachedCity.rc.Tekort > 0) {
+			if (AttachedCity.rc.TekortType == Player.Grondstof.Voedsel)
 				sT = Resources.Load<Sprite> ("Sprites/voedsel");
-			else if (c.rc.TekortType == Player.Grondstof.Textiel)
+			else if (AttachedCity.rc.TekortType == Player.Grondstof.Textiel)
 				sT = Resources.Load<Sprite> ("Sprites/textiel");
-			else if (c.rc.TekortType == Player.Grondstof.Steenkool)
+			else if (AttachedCity.rc.TekortType == Player.Grondstof.Steenkool)
 				sT = Resources.Load<Sprite> ("Sprites/steenkool");
 			else
 				sT = null;
@@ -44,12 +42,12 @@ public class Stats_City : MonoBehaviour {
 		else
 			sT = null;
 
-		if (c.rc.Overschot > 0) {
-			if (c.rc.OverschotType == Player.Grondstof.Voedsel)
+		if (AttachedCity.rc.Overschot > 0) {
+			if (AttachedCity.rc.OverschotType == Player.Grondstof.Voedsel)
 				sO = Resources.Load<Sprite> ("Sprites/voedsel");
-			else if (c.rc.OverschotType == Player.Grondstof.Textiel)
+			else if (AttachedCity.rc.OverschotType == Player.Grondstof.Textiel)
 				sO = Resources.Load<Sprite> ("Sprites/textiel");
-			else if (c.rc.OverschotType == Player.Grondstof.Steenkool)
+			else if (AttachedCity.rc.OverschotType == Player.Grondstof.Steenkool)
 				sO = Resources.Load<Sprite> ("Sprites/steenkool");
 			else
 				sO = null;
