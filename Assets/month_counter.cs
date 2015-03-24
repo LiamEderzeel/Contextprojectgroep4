c54b9fc69b48af919055b311ace88a7d208f39f9
ignazkevenaar <ignazkevenaar@gmail.com>
2015-03-24 13:02:18 +0100

    gamecounter

---
+++ Assets/month_counter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class month_counter : MonoBehaviour {
+
+	private int Month = 0;
+	private int SecondsPerMonth = 15;
+
+	private float c = 0;
+
+	private bool timerRunning = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Player.GameState == Player.gameState.Ingame) {
+			c += Time.deltaTime * 1f;
+			if (c >= SecondsPerMonth) {
+				c = 0;
+				Month++;
+			}
+			if (Month > 11) {
+				Debug.Log ("gewonnen");
+			}
+			else
+			{
+				Debug.Log (Month);
+			}
+		}
+
+	}
+}
