8319fdb44fae27990e18a287a26324da42fbf118
ignazkevenaar <ignazkevenaar@gmail.com>
2015-03-29 23:39:52 +0200

    instructieslides

--- Assets/Scripts/Instructions.cs
+++ Assets/Scripts/Instructions.cs
@@ -3,17 +3,30 @@ using System.Collections;
 
 public class Instructions : MonoBehaviour {
 
+	int counter = 3;
+
 	dialogswitch dia1;
 	dialogswitch dia2;
 
+	GameObject dia3;
+	GameObject dia4;
+	GameObject dia5;
 
 
 	// Use this for initialization
 	void Start () {
 		dia1 = transform.FindChild ("dia1").gameObject.GetComponent<dialogswitch> ();
 		dia2 = transform.FindChild ("dia2").gameObject.GetComponent<dialogswitch> ();
+
+		dia3 = transform.FindChild ("dia3").gameObject;
+		dia4 = transform.FindChild ("dia4").gameObject;
+		dia5 = transform.FindChild ("dia5").gameObject;
+
 		//dia1.gameObject.SetActive (false);
 		dia2.gameObject.SetActive (false);
+		dia3.gameObject.SetActive (false);
+		dia4.gameObject.SetActive (false);
+		dia5.gameObject.SetActive (false);
 
 		Player.sInstructions = this.gameObject;
 		Player.sInstructions.SetActive (false);
@@ -25,12 +38,29 @@ public class Instructions : MonoBehaviour {
 			if (!dia1 && !dia2.gameObject.activeSelf) {
 				dia2.gameObject.SetActive (true);
 			}
+		} else if (counter < 0) {
+			counter = 3;
 		}
+
+		if (counter == 3 && !dia3.activeSelf)
+			dia3.SetActive (true);
+		else if (counter != 3 && dia3.activeSelf)
+			dia3.SetActive (false);
+		if (counter == 4 && !dia4.activeSelf)
+			dia4.SetActive (true);
+		else if (counter != 4 && dia4.activeSelf)
+			dia4.SetActive (false);
+		if (counter == 5 && !dia5.activeSelf)
+			dia5.SetActive (true);
+		else if (counter != 5 && dia5.activeSelf)
+			dia5.SetActive (false);
 	}
 
 	void OnMouseDown()
 	{
-		if (!dia2) {
+		if (counter < 5) {
+			counter++;
+		} else {
 			Player.GameState = Player.gameState.Ingame;
 			this.gameObject.SetActive (false);
 		}
